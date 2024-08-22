46a968fba47c05a570795f02ad5c37bc22af7138
Kimberly Robles-Garcia <kimberlyrg978@gmail.com>
2024-08-22 04:13:55 +0000

    setup done

---
+++ w2/main.js
@@ -0,0 +1,18 @@
+// 1 Count the members of your household
+const myFamily = 2;
+// 2 Consider the size of your house
+const house = 7;
+// 3 Evaluate your food choices
+const food = 12;
+// 4 Examine your water consumption
+const water = 2;
+// 5 Determine how many household purchases you make each year
+const purchases = 6;
+// 6 Consider how much waste you produce
+const garbage = 40;
+// 7 Identify the amount of waste that you recycle 
+const recycle = 8;
+// 8 Tally up your transportation scores
+const myCar = 4;
+// 9 Add up your points
+const myTotal = myFamily + house + food + water + purchases + garbage + recycle + myCar

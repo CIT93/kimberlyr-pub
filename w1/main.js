4066b6bd5606925d9f679a683314c02b9f2e5128
Kimberly Robles-Garcia <kimberlyrg978@gmail.com>
2024-08-26 17:44:41 +0000

    Week 3 Attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+console.log("Hello from inside the main.js file");
+
+// let myVar;
+
+const myVar = 43;
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log('myVarType ${myVarType}');
+
+
+function runNow() {
+if(myVarType === "number") {
+        console.log('this line will run');
+} else {
+         console.log('this line will not run');   
+}
+}
+
+runNow ();
+runNow ();
+
+

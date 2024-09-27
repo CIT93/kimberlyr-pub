f8ebba3127b8ce2e2e714f9ac0159425566bd3b6
Kimberly Robles-Garcia <kimberlyrg978@gmail.com>
2024-09-27 02:12:27 +0000

    my attempt at the code, before I review other students code

--- w7/main.js
+++ w7/main.js
@@ -1,44 +1,11 @@
 import { renderTbl } from "./render.js";
+import {determineHouseSizePoints, determinePoints} from "./carbonfp.js";
 
 const FORM = document.getElementById("form");
 const OUTPUT = document.getElementById("output");
 const TBL = document.getElementById("tab-data");
 const cfpData = [];
 
-function determineHouseSizePoints(SizeOfHouse) {
-    let houseSizePoints = 0;
-  if (SizeOfHouse === "large") {
-    houseSizePoints = 10;
-} else if(SizeOfHouse === "medium") {
-    houseSizePoints = 7;
-} else if(SizeOfHouse === "small") {
-    houseSizePoints =  4;
-} else if(SizeOfHouse === "apt") {
-    houseSizePoints =  2;
-} 
-return houseSizePoints
-}
-
-
-function determinePoints(numberInHousehold) {
-    let houseHoldPoints = 0;
-  if (numberInHousehold === 1) {
-    houseHoldPoints = 14;
-} else if(numberInHousehold === 2) {
-    houseHoldPoints = 12;
-}  else if(numberInHousehold === 3) {
-    houseHoldPoints = 10;
-} else if(numberInHousehold === 4) {
-    houseHoldPoints = 8;
-} else if(numberInHousehold === 5) {
-    houseHoldPoints = 6;
-} else if(numberInHousehold === 6) {
-    houseHoldPoints = 4;
-} else if(numberInHousehold > 6) {
-    houseHoldPoints = 2;
-}
-return houseHoldPoints 
-}
 
 
 function start(houseHoldMembers, houseSize, firstName, lastName) {
--- w7/render.js
+++ w7/render.js
@@ -21,20 +21,30 @@ function renderTbl(data){
   const table = renderTblHeading();
   const tbody = document.createElement("tbody");
   const tr = document.createElement("tr");
-  const trTextArr = ["kimberly", 3, "Large", 20,];
-  trTextArr.forEach(function(text){
-      const td = document.createElement("td");
-      td.textContent = text;
-      tr.appendChild(td);
-  });
-const td = document.createElement("td");
-const btnEdit = document.createElement("button");
-const btnDel = document.createElement("button");
-btnEdit.textContent = "Edit";
-btnDel.textContent = "Del";
-td.appendChild(btnEdit);
-td.appendChild(btnDel);
-tr.appendChild(td);
+
+  data.forEach(function(obj){
+      
+      const Name = document.createElement("td");
+      const total = document.createElement("td");
+      Name.textContent = obj.firstname;
+      total.textContent = obj.cfpTotal;
+      tr.appendChild(Name);
+      tr.appendChild(total);
+      // const trTextArr = ["kimberly", 3, "Large", 20,];
+      // const td = document.createElement("td");
+      // td.textContent = text;
+      // tr.appendChild(td);
+      // I'm using this code but changing the vaule adn adding new code to fit with the data input
+  }); 
+
+// const td = document.createElement("td");
+// const btnEdit = document.createElement("button");
+// const btnDel = document.createElement("button");
+// btnEdit.textContent = "Edit";
+// btnDel.textContent = "Del";
+// td.appendChild(btnEdit);
+// td.appendChild(btnDel);
+// tr.appendChild(td);
 tbody.appendChild(tr);
 table.appendChild(tbody);
 console.log(table);
